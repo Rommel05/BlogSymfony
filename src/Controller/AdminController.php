531151f420c076da324ad484c07a81d96f936436
Ryze <rrll20220@gmail.com>
2024-10-16 21:34:04 +0200

    V7

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,9 +10,12 @@ use App\Form\ContactFormType;
 use App\Form\ImageFormType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AdminController extends AbstractController
 {
@@ -38,20 +41,50 @@ class AdminController extends AbstractController
     }
 
     #[Route('/admin/images', name: 'app_images')]
-    public function images(ManagerRegistry $doctrine, Request $request): Response
+    public function images(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
     {
         $image = new Image();
         $form = $this->createForm(ImageFormType::class, $image);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form->get('file')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+                // Move the file to the directory where images are stored
+                try {
+
+                    $file->move(
+                        $this->getParameter('images_directory'), $newFilename
+                    );
+                    $filesystem = new Filesystem();
+                    $filesystem->copy(
+                        $this->getParameter('images_directory') . '/'. $newFilename,
+                        $this->getParameter('portfolio_directory') . '/'.  $newFilename, true);
+
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $image->setFile($newFilename);
+            }
             $image = $form->getData();
             $entityManager = $doctrine->getManager();
             $entityManager->persist($image);
             $entityManager->flush();
         }
-        return $this->render('admin/images.html.twig', [
-        'form' => $form->createView()
-    ]);
+
+        $images = $doctrine->getRepository(Image::class)->findAll();
+
+        return $this->render('admin/images.html.twig', array(
+            'form' => $form->createView(),
+            'images' => $images
+        ));
     }
 
 
