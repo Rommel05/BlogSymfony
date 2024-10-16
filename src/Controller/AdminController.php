d841991da1ae1338452e2c7ca77d62e64868d145
Ryze <rrll20220@gmail.com>
2024-10-16 21:02:47 +0200

    V6

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Contact;
+use App\Entity\Image;
 use App\Form\CategoryFormType;
+use App\Form\ContactFormType;
+use App\Form\ImageFormType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,12 +16,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin/images', name: 'app_admin')]
-    public function index(): Response
-    {
-        return $this->render('admin/images.html.twig', []);
-    }
-
     #[Route('/admin/categories', name: 'app_categories')]
     public function categories(ManagerRegistry $doctrine, Request $request): Response
     {
@@ -38,6 +36,24 @@ class AdminController extends AbstractController
             'categories' => $categories,
         ));
     }
+
+    #[Route('/admin/images', name: 'app_images')]
+    public function images(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageFormType::class, $image);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($image);
+            $entityManager->flush();
+        }
+        return $this->render('admin/images.html.twig', [
+        'form' => $form->createView()
+    ]);
+    }
+
 
 
 }
